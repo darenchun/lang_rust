064af85ca944896528c7a45131f06cfb0e9ab63f
darenchun <darenchun@naver.com>
2022-04-26 15:23:56 +0900

    added contents in 'rust_struct_example' about methods and associated functions

--- rust_struct_example_program/src/main.rs
+++ rust_struct_example_program/src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::mpsc::Receiver;
+
 
 fn main() {
     /* 예시로 사각형의 넓이를 구하는 프로그램을 짜보도록 한다. */
@@ -25,9 +27,26 @@ fn main() {
         area3(&rec_2)
 
     );
+
     /* 파생 트레잇(derived trait)으로 유용한 기능 추가하기 */
-    
-    
+    let rec_3 : Rectangle = Rectangle { length: 44, width: 59 };
+    println!(
+        "The `area of the rectangle is {} square pixels",
+        area3(&rec_3)
+    );
+
+    println!("info about rec_1 {:#?}", rec_3);
+
+    /* method 문법 정의하기 : 구조체 내에서 유효한 함수를 내장시켜 편리하게 이용하기 */
+    let rec_4 : Rectangle = Rectangle{length: 50 , width : 50};
+    println!("the area of the rectanble is {}", rec_4.area());
+    let rec1 : Rectangle = Rectangle{length : 100, width : 50};
+    let rec2 : Rectangle = Rectangle { length: 40, width: 120 };
+    println!("can rec_1 hold rec_2? : {}", rec1.can_hold(&rec2));
+
+    /* 연관 함수 : impl 블록의 또 다른 기능은 기능을 구현하고 있는 자기 자신을 사용하지 않는 함수도 정의할 수 있다는 점이다. */
+    let rec3 = Rectangle::square(10);
+    println!("{:#?}", rec3);
 }
 
 
@@ -42,9 +61,28 @@ fn area2(dimensions : (u32, u32)) -> u32 {
 }
 
 /* 세 번째 예시 struct 및 사용 함수 : paramemter as struct*/
+#[derive(Debug)]
 struct Rectangle {
     length : u32,
     width : u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        return self.length * self.width
+    }
+
+
+    /* method : functions that uses implemented structure(self) */
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
+    
+    /* associated funtion : functions that are a part of the implementation but not using itself as contents */
+    fn square (size : u32) -> Rectangle {
+        return Rectangle { length : size, width : size };
+    }
+    
 }
 
 fn area3(rectangle: &Rectangle) -> u32 {
