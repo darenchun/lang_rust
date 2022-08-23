060f23456d6c8a9cab755eddcf41cfc8dcc1b2c5
darenchun <darenchun@naver.com>
2022-08-23 11:21:29 +0900

    commit

---
+++ rust_testing/tests/common/mod.rs
@@ -0,0 +1,5 @@
+
+/* doesn't appear in test results, sub-directories are not accepted by test-cargo */
+pub fn setup () {
+    println!("setup function has been called");
+}
--- rust_testing/tests/integration_test.rs
+++ rust_testing/tests/integration_test.rs
@@ -4,9 +4,10 @@ Convention in integration test, you need to think your code as an 'external libr
 This case we create a folder called 'test' and create an module isolated from your code and write test cases there.
 'Cargo' will make each of the files as an individual case.
  */
- use cargo;
-
+ use rust_testing;
+mod common;
 #[test]
-fn multification(){
-    assert_eq!(4, module_for_testing::it_adds_two(2));
-} 
+fn it_adds_two(){
+    common::setup();
+    assert_eq!(4, rust_testing::add_two(2));
+}
